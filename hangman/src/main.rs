6e3588634fd3a6e103d74555b9d9644ff067d3a1
katecpp <kasia.macias@gmail.com>
2016-03-12 10:51:39 +0100

    Function printing masked string added

--- hangman/src/main.rs
+++ hangman/src/main.rs
@@ -9,18 +9,32 @@ use rand::Rng;
 
 fn main() {
     println!("Hello, world!");
+
+    let mut discovered_letters = String::new();
     
-    let secret_line = read_input().expect("failed to read input data!");
-    read_guess();
+    let secret_line = read_input()
+                      .expect("Failed to read input data!");
+
+    let a = String::from("Kasia");
+    let b = String::from("a");
+    print_masked_string(secret_line, b);
+
+    let mut user_guess = read_guess();
+    // TODO: check if input is appearing in line...
+    // TODO: print as for each undiscovered char
+
 }
 
-fn read_guess() {
+fn read_guess() -> Result<String, io::Error>
+{
     println!("Please input your guess.");
 
     // TODO: read one char instead of line
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
     println!("You guessed: {}", guess);
+
+    Ok(guess)
 }
 
 fn read_input() -> Result<String, io::Error>
@@ -46,3 +60,17 @@ fn read_input() -> Result<String, io::Error>
     Ok(secret_line)
 }
 
+fn print_masked_string(input: String, mask: String)
+{
+    println!("Original string: {}. Mask: {}", input, mask);
+
+    for (u, c) in input.chars().enumerate()
+    {
+        print!("{} ",
+                if c == ' ' {c}
+                else if mask.contains(c) {c}
+                else {'_'});
+    }
+
+    print!("\n");
+}
